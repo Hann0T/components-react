28b9088c9058a9c240673ef5c460d7d024778197
hann0t <hansolivas1@gmail.com>
2022-01-05 08:38:43 -0500

    new component.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 import { DataTable } from './DataTable';
 import { SideBar } from './SideBar';
+import { StackedLayout } from './StackedLayout';
 
 const App = () => {
   return (
     <>
-      <SideBar />
-      <div className='pl-48 pt-16 min-h-screen'>
-        <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
-          <DataTable />
-        </div>
-      </div>
+      {/* <SideBar /> */}
+      {/* <div className='pl-48 pt-16 min-h-screen'> */}
+      {/*   <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'> */}
+      {/*     <DataTable /> */}
+      {/*   </div> */}
+      {/* </div> */}
+      <StackedLayout />
     </>
   );
 };
